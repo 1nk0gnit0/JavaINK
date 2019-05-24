b49294c1d76df0804c5a5c0f40dfd739475d604e
1nk0gnit0 <inkognito_1987@list.ru>
2019-05-24 16:09:15 +0900

    реализована работа исключений

--- src/Two/Main.java
+++ src/Two/Main.java
@@ -3,21 +3,21 @@ package Two;
 public class Main {
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
 
-       String[][] sizeErrorMatrix = {{"1", "1", "1", "1"}, {"1", "д", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}};
+       String[][] sizeErrorMatrix = {{"1", "1", "1", "1"}, {"1", "1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}};
 
-       String[][] dataErrorMatrix = {{"1", "1", "1", "1"}, {"1", "L", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}};
+       String[][] dataErrorMatrix = {{"l", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}};
 
         try {
             ConvertSum.sumConvert(sizeErrorMatrix);
-        }catch (MyArraySizeException e){
+        }catch (MyArrayDataException | MyArraySizeException e){
             System.out.println(e);
         }
-
-       /* try {
+        System.out.println();
+        try {
             ConvertSum.sumConvert(dataErrorMatrix);
-        }catch (MyArrayDataException e){
+        }catch (MyArrayDataException | MyArraySizeException e){
             System.out.println(e);
-        }*/
+        }
 
 
 
