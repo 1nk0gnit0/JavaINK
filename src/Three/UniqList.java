642d60086b21df2187c7584356be58309be6b4cb
1nk0gnit0 <inkognito_1987@list.ru>
2019-05-31 10:51:15 +0900

    Реализован вывод в консоль массива без повторений и подсчитана сумма повторений в массиве

--- src/Three/UniqList.java
+++ src/Three/UniqList.java
@@ -1,4 +1,27 @@
 package Three;
 
+
+import java.util.*;
+
 public class UniqList {
+    public static void main(String[] args) {
+
+        String[] arr = {"one","two","three","one","four","two","five","four","three","one",};
+
+        Set<String> unique = new HashSet<>(Arrays.asList(arr));
+
+        System.out.println(unique);
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s : arr) {
+           map.merge(s, 1, (a, b)->a + b);
+        }
+
+        System.out.println(map);
+
+
+
+
+    }
 }
